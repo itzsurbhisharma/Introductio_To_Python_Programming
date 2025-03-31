543bd589f68493866689df8c5ce6f456a6a57ef5
Surbhi Sharma <138388985+itzsurbhisharma@users.noreply.github.com>
2025-03-31 12:51:47 +0530

    Add files via upload

---
+++ Programs/Lab1a.py
@@ -0,0 +1,22 @@
+"""
+1. a) Develop a program to read the student details like Name, USN, and Marks in 
+three subjects. Display the student details, total marks and percentage with suitable 
+messages.
+"""
+
+student_name=input("Enter the name of the student: ")
+
+student_usn=input("Enter the usn of the student: ")
+
+print("Enter the marks of three subjects:")
+m1=float(input("Enter the marks of subject 1: "))
+m2=float(input("Enter the marks of subject 2: "))
+m3=float(input("Enter the marks of subject 3: "))
+
+total_marks=m1+m2+m3
+percentage=total_marks/3
+
+print("Student's Name: ",student_name)
+print("Student's USN: ",student_usn)
+print("Student's Total Marks: ",total_marks)
+print("Student's Percentage: ",percentage)
